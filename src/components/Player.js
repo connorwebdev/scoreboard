11b42894ba09344a1ef75c99948425b3e25106e5
Connor <connor@thefold.co.nz>
2023-09-10 14:25:52 +1200

    Adding project files

---
+++ src/components/Player.js
@@ -0,0 +1,36 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Counter from './Counter';
+import { Consumer } from './Context';
+
+class Player extends PureComponent {
+
+  static propTypes = {
+    index: PropTypes.number.isRequired
+  };
+
+  render() {
+    
+    const { index } = this.props;
+
+    return (
+      <div className="player">
+      <Consumer>
+        { ({ actions, players }) => {
+          return(
+            <span className="player-name">
+            <button className="remove-player" onClick={() => actions.removePlayer(players[index].id)}>✖</button>
+            { players[index].name }
+          </span>
+          )
+        }}
+      </Consumer>
+        
+  
+        <Counter index={index} />
+      </div>
+    );
+  }
+}
+
+export default Player;
